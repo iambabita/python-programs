ad5934519e85afcd9806fe8782c08ada5d23d2b4
iambabita <bb.babita.bhattarai@gmail.com>
2025-11-28 21:18:09 +0545

    day 3 is completed

---
+++ day 3.py
@@ -0,0 +1,11 @@
+#Hastag is known as comment.
+'''comment is a type of code'''
+''' Which is not executed'''
+"""Triple double and triple single quote is also a comment"""
+# ctrl+/ is for uncomment.
+print("HELLO WORLD!!",7)
+print(2222*823567)
+print("Hey!! this is so good \n and perfrct. I really love this")
+print("Black slash n\n is used for \" insert a new line.\"")
+print("HEY",6,7 , sep='~', end= "2050,\n" )
+print(" Everything is alright")

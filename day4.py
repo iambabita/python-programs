fea50b0b791093111c188f16779792fb106960bf
iambabita <bb.babita.bhattarai@gmail.com>
2025-11-29 20:40:24 +0545

    Day 4 completed

---
+++ day4.py
@@ -0,0 +1,20 @@
+#VARIABLES!!!
+a=1
+b=True
+c=4509873456
+print(b)
+print("A variable is a named storage location that holds data in a program.")
+print(100+120+50)
+print("Data type specifies the type of value a variable holds.")
+print(a+c)
+print("The type of a is" , type(a))
+print("The type of b is" , type(b))
+print("The type of c is" , type(c))
+f=1.1
+print("The type of f is" , type(f))
+s=complex(62,39)
+print("The type of s is" , type(s))
+list1=[23,42,34,56, [-4,6]]
+print(list1)
+dict1={"Name":"Hari","Age":"19","Class":"12"}
+print(dict1)

7e4ba2c5711e4b1fcc30851917b63741d2579c12
iambabita <bb.babita.bhattarai@gmail.com>
2025-11-30 19:05:56 +0545

    Calculator

---
+++ day5.py
@@ -0,0 +1,22 @@
+#OPERATORS!
+opeator=input("Enter operator (+ - * / %): ")
+a=float(input("Enter first num:"))
+b=float(input("Enter second num:"))
+if opeator=="+":
+ print(a+b)
+
+elif opeator=="-":
+ print(a-b)
+
+elif opeator=="*":
+ print(a*b)
+
+elif opeator=="/":
+ print(a/b)
+
+elif opeator=="%":
+ print(a%b)
+
+else:
+ print ("Invalid operator")
+

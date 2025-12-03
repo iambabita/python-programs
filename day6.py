bfd5d3cdb31e49e40fe5d6ce263f4359892ab5d1
iambabita <bb.babita.bhattarai@gmail.com>
2025-12-03 19:13:43 +0545

    day 6 completed

---
+++ day6.py
@@ -0,0 +1,34 @@
+ 
+ 
+z=(input("Enter your name:"))
+print("My name is",z)
+
+p=(input("Enter your age:"))
+print("My age is",p)
+
+s=(input("Enter your birthplace:"))
+print("My birthplace is" ,s)
+
+q=(input("Enter your Father's name:"))
+print("My Father's name is",q)
+
+r=(input("Enter your contact number:"))
+print("My contact number is",q)
+
+a=(input("Enter your college name:"))
+print("My college name is",a)
+
+b=(input("Your hobbies are:"))
+print("I like to",b)
+
+
+
+x=(input("Enter first num:"))
+y=(input("Enter second number:"))
+print ( x + y )
+print(int(x)+int(y))
+print(float(x)+float(y))
+
+print(int(x)/int(y))
+print(int(x)%int(y))
+
--- list.py
+++ list.py
@@ -1,5 +1,9 @@
 num=[1,2,3,"Hari","Shyam","Gita"]
 print(num)
-a=(20,30,40,50,60,70)
+a=(20,70,3,50,40,80,300,35,10,25,5)
 print(a[0],)
+print(sum(a))
+print(sorted(a))
+c=[y*y for y in range(10)]
+print=("square of c")
 

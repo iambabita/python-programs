84d464cc210186cb0e0224cd7ebf6f1d63ba6012
iambabita <bb.babita.bhattarai@gmail.com>
2025-12-06 20:50:03 +0545

    day 9 completed

---
+++ day9.py
@@ -0,0 +1,44 @@
+a=int(input("Enter your age:"))
+print("Your age is :",a)
+
+print(a<18)
+
+print(a>18)
+print(a<=18)
+print(a>=18)
+print(a==18)
+print(a!=18)
+if a>=18:
+    print("You can drive")
+
+else:
+    print("You cannot drive")
+
+Appleprice = 190
+money=300
+
+if Appleprice<=190:
+    print("Apple can buy")
+else:
+    print("Apple can't buy")
+
+if money-Appleprice<190:
+    print("NO MONEY!!") 
+else:
+    print("MONEY IS AVAILABLE")   
+
+num= int(input("Enter the value:"))
+if num<0:
+    print("Number is negative")
+elif (num==0):
+    print("Number is zero")
+elif (num==-999):
+    print("Number is special")
+else:
+    print("Number is positive")
+
+    print("FUNNY!!")
+
+
+
+

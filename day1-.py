4212f2f34481c160edcb4a05e0cdf74cc91d4bb4
iambabita <bb.babita.bhattarai@gmail.com>
2025-12-07 21:44:07 +0545

    day 10 completed

---
+++ day1-.py
@@ -0,0 +1,22 @@
+names="Sita, Gita, Hari"
+print(names[0:4])
+print(len(names))
+
+
+Fruites="Pineapple"
+len1=len(Fruites)
+print("Pineapple is my" ,len1 , "Favroiute fruit.")
+pineapplelen=len(Fruites)
+print(Fruites[0:5])
+print(Fruites[1:6])
+print(Fruites[2:4])
+print(Fruites[0:-3])
+print(Fruites[-3:-1])
+
+
+nm="Sweety"
+print(nm[-4:-2])
+
+Alphabets="WXYZA"
+for i in Alphabets:
+    print(i)

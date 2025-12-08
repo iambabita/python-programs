a3567ce6f6abedf0ba9dc7200a2315132a6a0956
iambabita <bb.babita.bhattarai@gmail.com>
2025-12-08 21:09:50 +0545

    day 11 completed

---
+++ day11.py
@@ -0,0 +1,38 @@
+nm="Shyam!!"
+print(len(nm))
+print(nm)
+print(nm. upper())
+print(nm.lower())
+print(nm.rstrip("i"))
+print(nm.replace ("Shyam", "Sweta"))
+print(nm.split(" ") )
+blogHeading= "introduction to python"
+print(blogHeading.capitalize())
+
+str1="Welcome to python"
+print(len(str1))
+print(str1.center(50))
+print(nm.count("Sweta"))
+print(str1 . endswith("!!!"))
+
+str1="Welcome to python"
+print(str1.endswith("to",4,6))
+
+str1="He's name is micheal. He is such a responsible man"
+print(str1.find("is"))
+print(str1.index("h"))
+
+str2="Thisisveryinteresting"
+print(str2.isalnum())
+print(str2.isalpha())
+print(str2.islower())
+
+str3="Happy birthday to you\n"
+print(str3)
+print(str3.isprintable())
+print(str3.isspace())
+print(str3.swapcase())
+print(str3.title())
+
+
+

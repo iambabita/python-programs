c11c3f768e53ba2725a8bc2d9c18b7e4ffa1941b
iambabita <bb.babita.bhattarai@gmail.com>
2025-12-09 21:03:00 +0545

    match case

---
+++ day12.py
@@ -0,0 +1,29 @@
+x=int(input("Enter the value of x:"))
+match x:
+    case 0:
+        print("x is zero")
+    case 4:
+        print("case is 4")
+    case _ if x !=90:
+        print(x , "x is not 90")
+    case _ if x!=80:
+        print(x, "is not 80")
+    case _ if x !=20:
+        print(x, "is not 20")
+    case _ :
+        print(x)
+
+
+
+ 
+
+
+ 
+ 
+
+
+
+
+
+    
+

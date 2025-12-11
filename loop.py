db8a61960d7537448fa54cf7f8abe9fe74882f9f
iambabita <bb.babita.bhattarai@gmail.com>
2025-12-11 21:16:16 +0545

    For loop

---
+++ loop.py
@@ -0,0 +1,19 @@
+name="Krishna"
+for i in name:
+    print(i)
+    if (i=="b"):
+        print("The way i looked at that object")
+    
+
+    colours=["Red" ,"Brown", "Pink", "White", "Black"]
+    for colours in colours:
+        print(colours)
+        for z in colours:
+         print(z)
+
+
+
+for r in range(200 , 1 , 20):
+   print(r+1)
+
+

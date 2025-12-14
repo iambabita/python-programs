3b7174d65e1ca0707548a9ffa6e090598dcc851c
iambabita <bb.babita.bhattarai@gmail.com>
2025-12-14 20:55:33 +0545

    sum using loop

--- heart.py
+++ heart.py
@@ -1,17 +1,19 @@
 import turtle
-turtle.speed(3)
+turtle.speed(16)
 turtle.bgcolor('black')
+turtle.color('red','pink')
 turtle.pensize(3)
-def func():
+def curve():
  for i in range(200):
   turtle.right(1)
   turtle.forward(1)
-turtle.color('red','pink')
 turtle.begin_fill()
 turtle.left(140)
 turtle.forward(111.65)
-func()
+curve()
+turtle.left(120)
+curve()
 turtle.forward(111.65)
 turtle.end_fill()
 turtle.hideturtle()
-turtle.done
+turtle.done()

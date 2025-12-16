7638f84326434ec203153c301f976846ac01e786
iambabita <bb.babita.bhattarai@gmail.com>
2025-12-16 21:02:40 +0545

    day 18

---
+++ whileloop.py
@@ -0,0 +1,20 @@
+i=int(input("Enter the number:"))
+for i in range(3):
+    print(i)
+
+i=0
+while(i<=34):
+    i=int(input("Enter the number:"))
+    print(i)
+    
+
+
+print("Done with loop")
+
+count=5
+while(count<0):
+    print(count)
+    count=count-1
+else:
+    print("Outside the loop")
+

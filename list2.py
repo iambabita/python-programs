8997f2f1400d51226243489f8b31ebcbb2ca86d5
iambabita <bb.babita.bhattarai@gmail.com>
2025-12-25 21:20:40 +0545

    day 24

---
+++ list2.py
@@ -0,0 +1,36 @@
+marks=[3,5,6]
+print(marks)
+ 
+ 
+print(type(marks))
+colors=("Red","Green","Blue","Pink","White","Black")
+print(colors)
+print(colors[0])
+print(colors[1])
+
+print(marks[-3])  #Negative index
+ 
+print(marks[5-3])
+print(marks[2])
+if 3 in marks:
+    print("Yes")
+else:
+    print("NO")
+    if "ed" in colors:
+        print("Yes")
+    else:
+        print("No")
+        
+
+print(marks)
+print(marks[1:-1])
+print(marks[1:4:5])
+
+lst=[i for i in range(20)]
+print(lst)
+lst1=[i*i for i in range(4)]
+print(lst1)
+
+
+
+

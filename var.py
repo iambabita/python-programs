57d757d758460c04c7e1de41517b8678241b78c8
iambabita <bb.babita.bhattarai@gmail.com>
2025-12-26 20:45:56 +0545

    variables

---
+++ var.py
@@ -0,0 +1,26 @@
+ 
+name = "Babita"
+age = 20
+height = 5.2
+
+# Using variables
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+
+# Variables
+a = 10
+b = 5
+
+
+sum = a + b
+diff = a - b
+mul = a * b
+div = a / b
+
+
+print("Addition:", sum)
+print("Subtraction:", diff)
+print("Multiplication:", mul)
+print("Division:", div)
+

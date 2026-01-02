57aae6b60318c3e0066541720863cf822e59c770
iambabita <bb.babita.bhattarai@gmail.com>
2026-01-02 20:22:53 +0545

    day 25

---
+++ elif.py
@@ -0,0 +1,22 @@
+name="superman!!"
+print("Hello,"+ name)
+banana='''He said,
+he wants to eat an banana
+"banana is my favrouite fruit"'''
+print(banana)
+print(name[5])
+print(18*2)
+for character in banana:
+  print(character)
+  for i in range(1,12):
+    print (i)
+    for i in range(1,4):
+      if i%2==0:
+        print(i)
+        i=1
+        total=0
+        while i<=10:
+          total+=i
+          i+=1
+          print("sum=",total)
+ 
---
+++ listmethod2.py
@@ -0,0 +1,18 @@
+fruits = ["apple", "banana", "orange"]
+fruits.pop(1)     # removes banana
+print(fruits)
+
+fruits.pop()      # removes last item
+print(fruits)
+
+
+fruits = ["apple", "banana"]
+fruits.clear()               #removes everythong 
+print(fruits)         
+
+
+fruits = ["apple", "banana"]
+new_fruits = fruits.copy()
+print(new_fruits)
+
+

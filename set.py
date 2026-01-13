768e57b3e004b893b67637f567bd537b9f1dfec0
iambabita <bb.babita.bhattarai@gmail.com>
2026-01-13 18:32:42 +0545

    Set

---
+++ set.py
@@ -0,0 +1,21 @@
+p={2,4,6,7,6,2}
+print(p)
+intro=("Meena",18,12,False,5.6)
+print(intro)
+name=set()
+print(type(name))
+
+for value in intro:
+    print(value)
+
+loc=("Home","Address","Way","Door")
+if "Address" in loc:
+ print("Visited")
+
+A={4,3,5,8,4,}
+B={23,2,3,4,9,0}
+print(A&B)
+print(A-B)
+print(B-A)
+
+

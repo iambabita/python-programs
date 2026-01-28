e2743dd2fed8ca54feedca95487939b63acae50b
iambabita <bb.babita.bhattarai@gmail.com>
2026-01-28 21:31:46 +0545

    Enumerate function

---
+++ enumerate_function.py
@@ -0,0 +1,14 @@
+marks=[99,88,23,48,39,55]
+#for mark in marks:
+   # print(mark)
+   # index=0
+    #if index==3:
+     #   print("Everything is alright")
+      #  index+=1
+for index , mark in enumerate(marks , start=3):
+    print(mark)
+    if index ==3:
+        print("Sita , Beautiful")
+        index+=1
+
+        

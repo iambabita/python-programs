0041fa5fbc7fa660f099114cbbc81ad58399650b
iambabita <bb.babita.bhattarai@gmail.com>
2026-02-17 17:40:33 +0545

    task manager

---
+++ task_m.py
@@ -0,0 +1,34 @@
+tasks = []
+
+while True:
+    print("\n1. Add Task")
+    print("2. View Tasks")
+    print("3. Remove Task")
+    print("4. Exit")
+
+    choice = input("Enter choice: ")
+
+    if choice == "1":
+        task = input("Enter new task: ")
+        tasks.append(task)
+        print("Task added!")
+
+    elif choice == "2":
+        print("\nYour Tasks:")
+        for i, task in enumerate(tasks, start=1):
+            print(i, ".", task)
+
+    elif choice == "3":
+        task_number = int(input("Enter task number to remove: "))
+        if 0 < task_number <= len(tasks):
+            removed = tasks.pop(task_number - 1)
+            print("Removed:", removed)
+        else:
+            print("Invalid task number")
+
+    elif choice == "4":
+        print("Goodbye!")
+        break
+
+    else:
+        print("Invalid choice")

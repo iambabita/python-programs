3292ad0d824569c8ef876a5089625de855de8552
iambabita <bb.babita.bhattarai@gmail.com>
2026-03-02 21:26:11 +0545

    decorators

---
+++ decorators.py
@@ -0,0 +1,18 @@
+def hello():
+ print("Hello world")
+
+def greet(fx):
+    def mfx(*args ,**kwargs):
+        print("Good Morning")
+        fx(*args , **kwargs)
+        print("Thanks for using this functions")
+        return mfx
+@greet 
+def hello():
+   print("Hello world")
+
+def add(a, b):
+    print(a + b)
+
+hello()
+greet (add) (1 , 2)
